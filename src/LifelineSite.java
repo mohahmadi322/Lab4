325df8bfb1c38a2f1a8157726c3ee3d41dd77ee5
mohahmadi322 <mohammadahmadi3@cmail.carleton.ca>
2024-11-07 11:15:11 -0500

    refactored LifelineSite

--- src/LifelineSite.java
+++ src/LifelineSite.java
@@ -1,17 +1,9 @@
-public class LifelineSite {
-    private double _units;
-    private double _rate;
+public class LifelineSite extends Site{
 
     public LifelineSite(double _units, double _rate) {
-        this._units = _units;
-        this._rate = _rate;
+        super(_units, _rate);
     }
 
 
-    double getBillableAmount(){
-        double base = _units * _rate * 0.5;
-        double tax = base * Site.TAX_RATE * 0.2;
-        return base + tax;
 
-    }
 }
